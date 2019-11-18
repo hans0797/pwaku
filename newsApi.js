f13d3790e15a112e7fadf2ec080f7410233f0592
hans0797 <57375344+hans0797@users.noreply.github.com>
2019-11-18 10:22:32 +0700

    Update newsApi.js

--- newsApi.js
+++ newsApi.js
@@ -1,4 +1,4 @@
-const apiKey = '7f65a98cdbf1496088f8cb61e23fd2e8';
+const apiKey = '54f280ccaa1c4cd2a0aae244012f86b6';
 const topHeadlinesUrl =
   'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
 
